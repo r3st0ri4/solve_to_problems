542d7cd5a08384a6550da26086061fd0ec27bfe7
r3st0ri4 <hasanzadetuncay68@gmail.com>
2024-12-06 21:58:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("qq");
+let land = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0]
+
+for(i = 0; i <= land.lenth; i++) {
+  if(land[i] == 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
